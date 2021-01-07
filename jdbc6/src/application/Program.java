4e4b3318edb91be37f21fd109810862ca2f75077
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-01-07 11:29:33 -0300

    Comentado o trecho que houve o erro fake para mostrar a alteracao

--- jdbc6/src/application/Program.java
+++ jdbc6/src/application/Program.java
@@ -24,18 +24,18 @@ public class Program {
 			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2100 WHERE DepartmentId = 1");
 			
 			//Criando um erro falso para simular um problema na transação
-			int x = 1;
+			/*int x = 1;
 			if( x < 2) {
 				throw new SQLException("falso erro!");
-			}
+			}*/
 			
 			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
 			
 			//Aqui é para confirmar que ambas as operações acabou
 			conn.commit();
 			
-			System.out.println("rows 1" + rows1);
-			System.out.println("rows 2" + rows2);
+			System.out.println("rows 1 " + rows1);
+			System.out.println("rows 2 " + rows2);
 		}
 		catch(SQLException e){
 			//e.printStackTrace();
